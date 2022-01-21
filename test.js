2acfa5538f5c63fca1c0b0d714665ba1f31225b0
mooseon <whomsp@gmail.com>
2022-01-21 13:46:58 +0900

    test.js file

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
-function even(){  
-  let array = [];
+function even() {
+	let array = [];
 
-  for (let i = 1; i < 51; i++) {
-    if (i % 2 === 0) {
-      array.push(i)
-    } 
-  return array;
-} 
+	for (let i = 1; i < 51; i++) {
+		if (i % 2 === 0) {
+			array.push(i);
+		}
+		return array;
+	}
+}
